467614869303a483df527306fe976cfd94416f78
lee-wood <leemaewood@gmail.com>
2020-02-20 19:19:01 -0500

    index.js and server built

---
+++ auth/auth-middleware.js
@@ -0,0 +1,15 @@
+//import all the things.
+//write both restricted middleware and department-check middleware here
+
+function restricted(req, res, next) {
+
+}
+
+function checkDept(dept) {
+//return function(req, res, next) {...}
+}
+
+module.exports = {
+  checkDept,
+  restricted
+}
