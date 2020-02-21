3071782f9ab3fa69fa0aa60529281d415e798ecc
lee-wood <leemaewood@gmail.com>
2020-02-20 20:45:10 -0500

    middleware implemented, to view user list there must be an active token presnet in header

--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -1,12 +1,36 @@
-//import all the things.
-//write both restricted middleware and department-check middleware here
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets =require('./secrets.js');
+const Users = require('../users/user-model.js');
 
 function restricted(req, res, next) {
 
-}
+  const token = req.headers.authorization;
+
+  if(req.decodedJwt) {
+    next();
+  }else if(token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedJwt) => {
+      if(err) {
+        res.status(401).json({ message: "You shall not pass!" });
+      }else {
+        req.decodedJwt = decodedJwt;
+        next();
+      }
+    })
+  }else {
+    res.status(401).json({ message: "Sorry, you're not authorized..."});
+  }
+};
 
 function checkDept(dept) {
-//return function(req, res, next) {...}
+  return function(req, res, next) {
+    if(req.decodedJwt.department && req.decodedJwt.department.includes(dept)) {
+      next();
+    }else {
+      res.status(403).json({ message: "You don't have access to this information."});
+    }
+  }
 }
 
 module.exports = {
--- users/user-router.js
+++ users/user-router.js
@@ -1,13 +1,22 @@
 const router = require('express').Router();
 
 const Users = require('./user-model.js');
+const midWare = require('../auth/auth-middleware.js')
 
-router.get('/', (req, res) => {
+router.get('/', midWare.restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+// router.get('/my-dept', midWare.restricted, midWare.checkDept(), (req, res) => {
+//   Users.find()
+//     .then(users => {
+//       res.json(users);
+//     })
+//     .catch(err => res.send(err));
+// });
 
 module.exports = router;
